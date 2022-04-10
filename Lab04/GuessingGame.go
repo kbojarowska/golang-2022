97114c62dbcf846e65263b41c3e4085a3e79dc8b
kbojarowska <kbojarowska@wp.pl>
2022-04-10 13:37:49 +0200

    Created third stage of guessing game

--- Lab04/GuessingGame.go
+++ Lab04/GuessingGame.go
@@ -8,35 +8,48 @@ import (
 )
 
 func main() {
-	var n string
-	rand.Seed(time.Now().UnixNano())
-	min := 0
-	max := 100
-	random := rand.Intn(max-min) + min
-
-	fmt.Println("Welcome to the guessing game!")
-	fmt.Println("Now you will be guessing the number I choosed")
-	fmt.Println("If you want to quit game - type \"Exit\"")
-	fmt.Println("Type number: ")
-	fmt.Scan(&n)
-
-	attempts := 0
+end:
 	for {
-		attempts++
-		number, error := strconv.Atoi(n)
-		_ = error
-		if n == "Exit" {
-			fmt.Println("Goodbye :(")
-			break
-		} else if number > random {
-			fmt.Println("Your guess is bigger than the number. Try again")
-			fmt.Scan(&n)
-		} else if number < random {
-			fmt.Println("Your guess is smaller than the number. Try again")
-			fmt.Scan(&n)
-		} else {
-			fmt.Println("Congratulations you win after", attempts, "attempts")
-			break
+		var n string
+		rand.Seed(time.Now().UnixNano())
+		min := 0
+		max := 100
+		random := rand.Intn(max-min) + min
+
+		fmt.Println("Welcome to the guessing game!")
+		fmt.Println("Now you will be guessing the number I choosed")
+		fmt.Println("If you want to quit game - type \"Exit\"")
+		fmt.Println("Type number: ")
+		fmt.Scan(&n)
+
+		attempts := 0
+		for {
+			attempts++
+			number, error := strconv.Atoi(n)
+			_ = error
+			if n == "Exit" {
+				fmt.Println("Goodbye :(")
+				break end
+			} else if number > random {
+				fmt.Println("Your guess is bigger than the number. Try again")
+				fmt.Scan(&n)
+			} else if number < random {
+				fmt.Println("Your guess is smaller than the number. Try again")
+				fmt.Scan(&n)
+			} else {
+				fmt.Println("Congratulations you win after", attempts, "attempts")
+				fmt.Println("Do you want to play again? [T/N]")
+				var again string
+				fmt.Scan(&again)
+				if again != "T" && again != "N" {
+					fmt.Println("Bad input")
+					fmt.Scan(&again)
+				} else if again == "N" {
+					break end
+				} else if again == "T" {
+					break
+				}
+			}
 		}
 	}
 }
